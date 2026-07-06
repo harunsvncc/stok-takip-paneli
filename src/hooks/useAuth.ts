bd471d241f8da362cacf4aaf15a5a03642b32caf
harunsvncc <harungfb-@hotmail.com>
2026-07-06 16:55:39 +0300

    fix: useAuth'teki event hatası düzeltildi

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -32,7 +32,7 @@ export function useAuth() {
     fetchUser()
 
     const { data: { subscription } } = supabase.auth.onAuthStateChange(
-      async (event, session) => {
+      async (_event, session) => {
         try {
           setUser(session?.user || null)
           
